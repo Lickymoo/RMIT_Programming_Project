8d536207b5bfa6718005a7a9185b62c3ad31dd65
Buby <mitchgertrude222@gmail.com>
2021-10-02 17:22:22 +1000

    added canMove function

--- src/main/java/rmit/f21b/game/navigation/MapSection.java
+++ src/main/java/rmit/f21b/game/navigation/MapSection.java
@@ -4,16 +4,9 @@ public class MapSection {
 
     public final String name;
     public final String description;
+
     public MapSection(String name,String description){
         this.name = name;
         this.description = description;
-
-
     }
-
-
-
-
-
-
 }
--- src/main/java/rmit/f21b/game/navigation/NavigationMan.java
+++ src/main/java/rmit/f21b/game/navigation/NavigationMan.java
@@ -3,35 +3,41 @@ package rmit.f21b.game.navigation;
 import rmit.f21b.game.Player;
 
 public class NavigationMan {
-        // actual map size
-    MapSection [] [] map = new MapSection [5] [5];
+    // actual map size
+    static final int maxX = 5;
+    static final int maxY = 5;
+
+    private MapSection[][] map = new MapSection[maxX][maxY];
 
     public void moveNorth(Player player){
         int y = player.getPlayerLocationY();
-            player.setPlayerLocationY(y+1);
-
+        player.setPlayerLocationY(y+1);
     }
 
     public void moveSouth(Player player){
         int y = player.getPlayerLocationY();
         player.setPlayerLocationY(y-1);
-
     }
 
     public void moveEast(Player player){
         int x = player.getPlayerLocationX();
         player.setPlayerLocationX(x+1);
-
     }
 
     public void moveWest(Player player){
         int x = player.getPlayerLocationX();
         player.setPlayerLocationY(x-1);
-
     }
 
+    public boolean canMove(int x, int y){
+        if(x < 0 || y < 0 || x >= maxX || y >= maxY)
+            return false;
 
+        if(map[x][y] == null)
+            return false;
 
+        return true;
+    }
 
 
 
